23dac5cf8567fcfc1cb94e372629cc3cbdf61936
BGMP <jose@bgmp.cl>
2022-02-19 04:10:39 -0300

    Implement new version system

--- data.rb
+++ data.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
+TIME = Time.now
+
 module RVACars
   NAME = 'rva_cars'
   DESCRIPTION = "Re-Volt America's Cars Pack"
-  VERSION = '1.0000'
+  VERSION = "#{TIME.year.digits[0]}#{TIME.year.digits[1]}.#{TIME.month}#{TIME.day}"
   URL = 'https://distribute.revolt-america.com/rva/rva_cars.zip'
 end
