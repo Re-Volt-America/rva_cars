0426d3d2005d4ca5723a27e408bfaf79c3186aed
unknown <pichubritos@gmail.com>
2022-05-07 02:20:36 -0300

    updated data.rb versions

--- data.rb
+++ data.rb
@@ -4,8 +4,8 @@ module RVACars
   NAME = 'rva_cars'
   DESCRIPTION = "Re-Volt America's Cars Pack"
   YEAR = 22
-  MONTH = 3
-  DAY = 21
+  MONTH = 5
+  DAY = 7
   VERSION = "#{YEAR}.#{MONTH < 10 ? "0#{MONTH}" : MONTH}#{DAY}"
   URL = 'https://distribute.revolt-america.com/rva/rva_cars.zip'
 end
