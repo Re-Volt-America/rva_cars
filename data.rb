a6daabde9ad0262e055070fa1c7fce1e72b00902
José Benavente <jose@bgmp.cl>
2022-08-17 16:36:50 -0400

    Update to 22.0817a-1

--- data.rb
+++ data.rb
@@ -4,9 +4,9 @@ module RVACars
   NAME = 'rva_cars'
   DESCRIPTION = "Re-Volt America's Cars Pack"
   YEAR = 22
-  MONTH = 5
-  DAY = 9
-  REVISION = 4
+  MONTH = 8
+  DAY = 17
+  REVISION = 1
   SUFFIX = 'a'
   VERSION = "#{YEAR}.#{MONTH < 10 ? "0#{MONTH}" : MONTH}#{DAY}#{SUFFIX}-#{REVISION}"
   URL = 'https://distribute.revolt-america.com/rva/rva_cars.zip'
