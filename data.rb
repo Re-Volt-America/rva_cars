4955a6bb93d70b5980559ecbb22a572b51b5a772
BGMP <jose@bgmp.cl>
2022-09-10 22:51:07 -0300

    Bump revision

--- data.rb
+++ data.rb
@@ -6,7 +6,7 @@ module RVACars
   YEAR = 22
   MONTH = 8
   DAY = 17
-  REVISION = 2
+  REVISION = 3
   SUFFIX = 'a'
   VERSION = "#{YEAR}.#{MONTH < 10 ? "0#{MONTH}" : MONTH}#{DAY}#{SUFFIX}-#{REVISION}"
   URL = 'https://distribute.revolt-america.com/rva/rva_cars.zip'
